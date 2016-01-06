5a672137791e01e25d91fea29de96f6ab3df9561
ryszard314159 <ryszard@czerminski.net>
2016-01-06 11:42:23 -0500

    window FFT added

--- R/sim-eeg.R
+++ R/sim-eeg.R
@@ -1,3 +1,16 @@
+if (0) {
+  x <- 1:9
+  nwin <- 2
+  n <- nwin + 1
+  m <- NULL
+  for (i in 1:n) m <- rbind(m, rep(i, n))
+  m <- rbind(m, (n+1):(2*n))
+  layout(m, widths=1, heights=1)
+  for (i in 1:max(m)) plot(x, main=i)
+  stop()
+}
+
+# source ('~/util/R/sim-eeg.R')
 # simulate EEG spectrum by superposing number of sinusoids with variable amplitudes and freq.
 # The purpose is to be able to create EEG-like signal with known characteristics
 # so the functions for EEG spectral analysis, etc. can be easily tested.
@@ -53,13 +66,22 @@ window_amp <- function(tott,         # total time in seconds
    return (v) 
 }
 
+plt.fft.power <- function(t, xt) {
+     dt <- max(t) - min(t)
+     f <- fft(xt)/length(xt)
+     p <- Mod(f[1:(length(f)/2)])
+     f <- (0:(length(p)-1))/dt # divide by time interval to get frq units right
+     plot(f, p, type='l', xlab=sprintf('frequency [Hz]'), ylab=sprintf('power'), xlim=c(0,1.1*max(frq)))
+}
+
 plt.amp <- function(amp,          # input amplitudes values
                     am,           # list with vectors of precomputed amplitudes for each time point
                     tott,         # total simulation time
                     sampling_frq,
                     units,        # list with units for time and amplitudes
+                    title=NULL,
                     debug=0) {
-     plot(1, type='n', xlim=c(0, tott), ylim=range(amp),
+     plot(1, type='n', xlim=c(0, tott), ylim=range(amp), main=title,
           xlab=sprintf('time [%s]', units$time), ylab=sprintf('amplitudes [%s]', units$amplitude))
      col <- rainbow(length(am))
      dt <- 1/sampling_frq
@@ -82,6 +104,7 @@ smooth <- function(x, n) {
    y[w] <- x[w]
    return (y)
 }
+
 
 simEEG <- function(tott,                # total simulation time in seconds
                    wint,                # window time in secods
@@ -132,39 +155,74 @@ simEEG <- function(tott,                # total simulation time in seconds
      v <- v + wam[[i]]*sin(w)
    }
    if (verbose) {
+     tt <- paste(round(frq,2), collapse=', ')
+     title <- paste('frq=', tt, '[Hz]', collapse='') 
      if (!is.null(fn)) png(fn)
-     old.par <- par(mfcol=c(2,1))
-     plt.amp(amp, wam, tott, sampling_frq, units, debug=0)
+     nwin <- tott / wint # number of windows (total time / window time)
+     n <- 2*nwin - 1
+     m <- NULL
+     for (i in 1:2) m <- rbind(m, rep(i, n))
+     m <- rbind(m, 2 + 1:n)
+     cat('m=\n'); print (m)
+     layout(m, widths=1, heights=1)
+     plt.amp(amp, wam, tott, sampling_frq, units, title, debug=0)
      plot(t, v, type='l', xlab=sprintf('time[%s]', units$time), ylab=sprintf('signal[%s]', units$amplitude))
-     par(old.par)
+     # plt.fft.power(t, v) # for the whole signal
+     #
+     # now make fft & plots for overlapping windows
+     #
+     no <- 2*nwin - 1     # number of overlapping windows (by half length)
+     np <- length(v)/nwin # number of points in one window
+     # nwin <- 2; np <- 4
+     starts <- (0:(2*nwin-2))*(np/2)
+     # points in i-th window: starts[i] + 1:np
+     for (i0 in starts) {
+        i <- i0 + 1:np
+        plt.fft.power(t[i], v[i]) 
+     }
+     cat('length(v)=', length(v), 'min(t), max(t)=', min(t), max(t), '\n')
+     cat('nwin, no, np =', nwin, no, np, 'starts=', starts, '\n')
      if (!is.null(fn)) dev.off(dev.cur())
    }
    return(v)
 } # end_simEGG
 
-# we can use these at some point for more realistic simulations
-# EEG frequency ranges [Hz]
-# from http://www.brainmaster.com/generalinfo/eegbands/eegbands.html
-eegFrq <- list(delta=c(0.1, 3), theta=c(4, 7), alpha=c(8,12), betaLow=c(12,15), betaMid=c(15,18),
-               betaHigh=c(19,99), # above 18 
-               gamma=c(39,41))
+get.eegFrq <- function() {
+  # we can use these at some point for more realistic simulations
+  # EEG frequency ranges [Hz]
+  # from http://www.brainmaster.com/generalinfo/eegbands/eegbands.html
+  eegFrq <- list(delta=c(0.1, 3),
+        	 theta=c(4, 7),
+                 alpha=c(8,12),
+                 betaLow=c(12,15),
+                 betaMid=c(15,18),
+                 betaHigh=c(19,99), # above 18 
+                 gamma=c(39,41))
+  return (eegFrq)
+}
 
+# by.window <- function(xt, sampling_frq, 
 
-set.seed(3)
-amp <- 2^(0:5)
+amp <- 1.5^(0:6)
 amp <- rev(amp)
-fbase <- sqrt(2)
+fbase <- sqrt(3)
 frq <- fbase^(0:(length(amp)-1))
 # frq <- round(1 + 4*runif(length(amp)),2)
 ph <- rep(0,length(amp))
-sampling_frq <- 256
-wint <- 8
+sampling_frq <- 256*1
+wint <- 32
 smooth_frac <- 1/16
 nsmooth <- 2^floor(log2(wint*sampling_frq*smooth_frac))+1
-tott <- 4*wint
+tott <- 3*wint
+
+set.seed(4)
 # wam <- window_amp(tott, wint, sampling_frq, amp, nsmooth, verbose=1)
-v <- simEEG(tott, wint, amp, frq, ph, sampling_frq, smooth_frac=smooth_frac, verbose=1, debug=0)
+fn.plt <- 'simEEG.png'
+# fn.plt <- NULL
+v <- simEEG(tott, wint, amp, frq, ph, sampling_frq, smooth_frac=smooth_frac, fn=fn.plt, verbose=1, debug=0)
 # v <- simEEG(tott, wint, amp, frq, ph, sampling_frq, smooth_frac=smooth_frac, verbose=1, fn='sim-eeg.png', debug=0)
 # plot(v, type='l')
 
 # source ('~/util/R/sim-eeg.R')
+
+stop('...')
